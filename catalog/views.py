134799f37b7e3789d6ba3d5b5daaa76a84031a9b
Zhenya Osokina <kapitan.kub@gmail.com>
2024-01-03 22:38:34 +0500

    доп задание под звездочкой, POST запрос

--- catalog/views.py
+++ catalog/views.py
@@ -5,6 +5,10 @@ def index(request):
     return render(request, 'catalog/index.html')
 
 def contacts(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'({email}):{message}')
     return render(request, 'catalog/contacts.html')
 
 def about_me(request):
