25b6a83acc06c5acd7847a6bf3eaf0a4f8ea7bb5
ZhenyaSok <145566585+ZhenyaSok@users.noreply.github.com>
2024-01-09 20:34:55 +0500

    дз к 20.1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+NULLABLE = {'null': True}
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Категория')
+    description = models.TextField(**NULLABLE)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование товара')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    photo = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Изображение')
+    price = models.FloatField(default=0, verbose_name='Цена за штуку')
+    overview = models.TextField(**NULLABLE, verbose_name='Описание')
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'{self.name} ({self.price})'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ('price',)
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,13 @@
 from catalog.views import contacts, index, about_me
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', index),
     path('contacts/', contacts),
     path('about_me/', about_me),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
