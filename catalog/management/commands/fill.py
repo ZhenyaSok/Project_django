1d915c06d5a00076e91ed61b7c0f8613dbae1914
ZhenyaSok <145566585+ZhenyaSok@users.noreply.github.com>
2024-01-11 19:58:36 +0500

    задание 6

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,35 +1,34 @@
-from django.core.management import  BaseCommand
-
-from catalog.models import Product
+from django.core.management import BaseCommand
+from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
-
     def handle(self, *args, **options):
-        products_list = [
-                      {
-                        "model": "catalog.product",
-                        "pk": 3,
-                        "fields": {
-                          "name": "Air Jordan 11 Retro",
-                          "category": 1,
-                          "photo": "catalog/air_IPZhuKI.jpg",
-                          "price": 28000.0,
-                          "overview": "модель ретро",
-                          "created": "2024-01-09T17:50:53Z",
-                          "updated": "2024-01-09T17:51:50.024Z"
-                        }
-                      }
-                    ]
-        for product_item in products_list:
-            Product.objects.create(**product_item)
+        product_list = [
+            {
+                "name": "Air Jordan 11 Retro",
+                "category": 1,
+                "photo": "catalog/air_IPZhuKI.jpg",
+                "price": 28000.0,
+                "overview": "модель ретро",
+                "created": "2024-01-09T17:50:53Z",
+                "updated": "2024-01-09T17:51:50.024Z"
+            }
+        ]
 
-        # products_for_create = []
-        # for product_item in products_list:
-        #     products_for_create.append(Product(**product_item))
-        #
-        # print(products_for_create)
-        #
-        # Product.object.bulk_create(products_for_create)
+        products_for_create = []
+        for product_item in product_list:
+            category_id = product_item.pop('category')
+            category = Category.objects.get(id=category_id)
+            product_item['category'] = category
+            products_for_create.append(
+                Product(**product_item)
+            )
+
+        Product.objects.bulk_create(products_for_create)
+
+
+
+
 
 
