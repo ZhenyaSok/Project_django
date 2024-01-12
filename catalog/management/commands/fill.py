bc8b2c3e9752b8fbf416816ba245566fd425f182
ZhenyaSok <145566585+ZhenyaSok@users.noreply.github.com>
2024-01-12 23:44:56 +0500

    Изменила логику в fill

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -58,31 +58,6 @@ class Command(BaseCommand):
 #         Product.objects.bulk_create(products_for_create)
 
 
-import json
-from django.core.management import BaseCommand
-from catalog.models import Category, Product
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        Category.objects.all().delete()
-        Product.objects.all().delete()
-
-        with open('data.json', "r", encoding="UTF-8") as file:
-            data = json.load(file)
-
-        products = []
-        category = []
-        for item in data:
-            if item["model"] == "catalog.product":
-                products.append(Product(**item['fields']))
-            elif item["model"] == "catalog.category":
-                category.append(Category(**item['fields']))
-
-
-        Product.objects.bulk_create(products)
-        Category.objects.bulk_create(category)
 
 
 
