e80132c7657a33bc2583e54d9167a0ecd9630f3a
ZhenyaSok <145566585+ZhenyaSok@users.noreply.github.com>
2024-01-12 17:36:35 +0500

    в работе, не работает статика

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
-from catalog.views import contacts, index, about_me
+from catalog.views import index_main  #, about_me, contacts
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', index),
-    path('contacts/', contacts, name='info'),
-    path('about_me/', about_me,),
+    path('', index_main),
+    # path('contacts/', contacts, name='info'),
+    # path('about_me/', about_me,),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'catalog/index.html')
+# def index(request):
+#     return render(request, 'catalog/index.html')
 
-def contacts(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'({email}):{message}')
-    return render(request, 'catalog/contacts.html')
+# def contacts(request):
+#     if request.method == 'POST':
+#         email = request.POST.get('email')
+#         message = request.POST.get('message')
+#         print(f'({email}):{message}')
+#     return render(request, 'catalog/contacts.html')
+#
+# def about_me(request):
+#     return render(request, 'catalog/about_me.html')
 
-def about_me(request):
-    return render(request, 'catalog/about_me.html')
+def index_main(request):
+    return render(request, 'catalog/index_main.html')
